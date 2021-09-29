2f348bd09bbb692b48a35c9a84679ca166ab60da
I'm no Superman <arey_dima@mail.ru>
2021-09-30 02:10:30 +0300

    1.1.3 final

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/preproject";
+    private static final String URL = "jdbc:mysql://localhost:3306/users";
     private static final String USER = "root";
     private static final String PASSWORD = "qwerty123";
 
