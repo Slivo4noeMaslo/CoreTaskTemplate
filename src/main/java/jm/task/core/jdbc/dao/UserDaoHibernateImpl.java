284c940e05bc26b2c83030791f546acfa2c69916
I'm no Superman <arey_dima@mail.ru>
2021-10-03 17:13:04 +0300

    1.1.4 final

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,9 +3,9 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -59,8 +59,6 @@ public class UserDaoHibernateImpl implements UserDao {
     public void removeUserById(long id) {
         try(Session session = Util.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
-//            String sql = "delete from users where id =" + id;
-//            session.createSQLQuery(sql).executeUpdate();
             session.delete(session.get(User.class, id));
             transaction.commit();
         } catch (Exception ex) {
@@ -72,7 +70,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> list = null;
         try(Session session = Util.getSessionFactory().openSession()) {
-            list = (List<User>) session.createSQLQuery("select * from users").list();
+            list = session.createSQLQuery("select * from users").list();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,15 +1,12 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.io.Serializable;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Users")
-public class User implements Serializable {
+public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
